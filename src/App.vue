b3f686c7450d242843d4186b7a7fccd0aaaa9058
jovisaib <jovisaib@gmail.com>
2019-11-04 11:27:18 +0100

    added preliminary description

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
   <div id="app">
-    <home/>
+    <home />
   </div>
 </template>
 
 <script>
-import Home from './components/Home.vue';
+import Home from "./components/Home.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Home
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  padding: 5vh 10vw;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+p {
+  font-size: 1.2em;
+  line-height: 1.4;
+  max-width: 40em;
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,46 @@
 <template>
-<div>
-  <h1>Hola.</h1>
-  <p style="font-weight:500; font-size:2.3em">
-    My name is Jose Vicente Sáez, jovisaib for shorten.
-  </p>
-</div>
+  <div>
+    <h1>Hola.</h1>
+    <p style="font-weight:500; font-size:2.0em">I'm Jose Vicente Sáez, jovisaib for shorten.</p>
+    <p>
+      I'm a engineer and designer involved within the lift industry, and my attention lies on the
+      intersection between data visualization, human/computer interaction and Machine Learning.
+
+      Please, check this out to get further information.
+    </p>
+
+    <div id="projects_content">
+      <div class="divider_line"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
 
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 a {
   color: #42b983;
+}
+
+#projects_content {
+  position: relative;
+  right: 0;
+  left: 0;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+.divider_line {
+  background-color: gray;
+  display: block;
+  height: 1px;
+  width: 100%;
+  border: 0;
+  margin-bottom: 8px;
+  margin-top: 8px;
+  opacity: 0.3;
 }
 </style>
