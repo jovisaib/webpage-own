e4f82bd53a936a461d92a4d972472a482f30f5b6
José Vicente Sáez <josevicente.saez@outsourcing.cepsa.com>
2021-06-30 12:40:03 +0200

    wip

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -30,9 +30,13 @@
 
     <div class="projects_content">
         <div class="divider_line"></div>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/TIRiJ0a3pFU?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/TIRiJ0a3pFU?controls=0&showinfo=0&modestbranding=1&autohide=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
 
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/ATZq6sw_N-U?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/ATZq6sw_N-U?controls=0&showinfo=0&modestbranding=1&autohide=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
         <div class="divider_line"></div>
     </div>
 </div>
