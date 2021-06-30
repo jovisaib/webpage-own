daf4f68571eb67d8aa8dfb522fe2e1c18e6920ee
José Vicente Sáez <josevicente.saez@outsourcing.cepsa.com>
2021-06-30 14:48:26 +0200

    wip

--- src/components/projects/DMXShow.vue
+++ src/components/projects/DMXShow.vue
@@ -1,10 +1,10 @@
 <template>
 <div style="text-align:center; width:100%;">
-    <div>
+    <div class="video-container">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/TIRiJ0a3pFU?controls=0&showinfo=0&modestbranding=1&autohide=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 
-    <div>
+    <div class="video-container">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/ATZq6sw_N-U?controls=0&showinfo=0&modestbranding=1&autohide=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 </div>
@@ -17,4 +17,21 @@ export default {
 </script>
 
 <style scoped>
+.video-container {
+    position: relative;
+    padding-bottom: 56.25%;
+    padding-top: 30px;
+    height: 0;
+    overflow: hidden;
+}
+
+.video-container iframe,
+.video-container object,
+.video-container embed {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
 </style>
