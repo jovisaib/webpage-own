c49c5d3d6c4fb77116947f6b70f75b677b562f9c
jvsaez <jovisaib@gmail.com>
2024-06-19 18:24:25 +0200

    wip

--- src/main.js
+++ src/main.js
@@ -2,19 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import { loadFonts } from './plugins/webfontloader';
-import { createRouter, createWebHistory } from 'vue-router'; // Assuming you are using vue-router
 import VueGtag from 'vue-gtag-next';
-import routes from './routes'; // Adjust this to your routes file
 
 loadFonts();
 
 const app = createApp(App);
 
-// Create a router instance (if using vue-router)
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
 
 app.use(vuetify);
 
@@ -22,6 +15,6 @@ app.use(VueGtag, {
   property: {
     id: 'G-KH8YDFLG7C' // Replace with your Google Analytics 4 measurement ID
   }
-}, router);
+});
 
 app.mount('#app');
